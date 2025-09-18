3880e640ea2de77537be276d48dd5035d8aff2be
Ben-cs50 <benedictodhiambo49@gmail.com>
2025-09-18 20:22:28 +0300

    test: add cypress report

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,30 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
 
+  reporter: 'cypress-mochawesome-reporter',
+  
+  e2e: {
     //baseUrl: 'http://192.168.1.56:5000',
     baseUrl: 'http://localhost:5000',
 
     setupNodeEvents(on, config) {
-
-      experimentalSessionAndOrigin: true
-  
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-  },
+
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports/html',
+      overwrite: true,
+      html: true,
+      json: true,
+      reportFilename: 'index',
+      charts: true,
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveHtml: true,
+      saveJson: true
+    }
+  }
 });
